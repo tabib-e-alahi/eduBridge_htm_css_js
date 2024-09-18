c72c3d56e5946ceb63c78819e8a5f46085ecac7b
tabib-e-alahi <etabib.alahi@gmail.com>
2024-09-18 22:18:10 +0600

    some responsive design added for mobile view only

--- js/popular.js
+++ js/popular.js
@@ -12,15 +12,14 @@ const displayPopularCourses = (popularCourses) => {
   const popularCardContainer = document.getElementById("popular-container");
   //   console.log(popularCourses);
   popularCourses.slice(0, 6)?.forEach((course) => {
-    console.log(course);
     const popularCard = document.createElement("div");
     popularCard.classList = `max-w-sm h-full p-4 border bg-white shadow-sm rounded-3xl font-sans  flex flex-col justify-between noto-sans-font`;
     popularCard.innerHTML = `
             <div class="flex gap-2 justify-start items-center mb-4 pl-6">
-                <img class="rounded-full size-6 outline outline-indigo-400" src=${course?.cover_image} alt="" />
+                <img class="rounded-full size-6 outline outline-indigo-400 " src=${course?.cover_image} alt="" />
                 <p class="font-medium font-serif">Yet to be announced</p>
             </div>
-                <img class="rounded-lg mb-2" src=${course?.cover_image} alt="" />
+                <img class="rounded-lg mb-2 w-[353px] h-[200px]" src=${course?.cover_image} alt="" />
             <div class=" flex-grow p-6  bg-white">
                 <div class="badge mb-3 bg-[#EDF8F9] text-[#17A2B8] rounded-none py-3 text-xs px-1.5 ">${course?.category}</div>
                 <div class="flex justify-start  gap-x-5">
